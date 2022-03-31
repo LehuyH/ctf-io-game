6358cb6e5004b343c188ba5dbe420cb4319ff29e
LehuyH <44150458+LehuyH@users.noreply.github.com>
2022-03-30 20:16:09 -0700

    Add/remove bodies within class.

--- apps/server/src/logic/ServerBody.ts
+++ apps/server/src/logic/ServerBody.ts
@@ -4,11 +4,14 @@ import Matter from 'matter-js';
 export class ServerBody{
     id: string
     body: Matter.Body
-    constructor(id: string, body: Matter.Body){
+    constructor(id: string, body: Matter.Body,world:Matter.World){
         this.id = id;
         this.body = body;
+        Matter.World.add(world,body);
     }
     sync(state: IState){}
     update(delta: number,state: IState){}
-    cleanup?: (state: IState) => void
+    cleanup(state: IState,world:Matter.World){
+        Matter.World.remove(world,this.body);
+    }
 }
