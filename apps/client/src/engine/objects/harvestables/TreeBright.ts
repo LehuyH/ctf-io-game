64997e76695a6b3f7a08873f75127a7ae1845fd9
LehuyH <44150458+LehuyH@users.noreply.github.com>
2022-04-04 13:16:09 -0700

    Use passed config

--- apps/client/src/engine/objects/harvestables/TreeBright.ts
+++ apps/client/src/engine/objects/harvestables/TreeBright.ts
@@ -2,24 +2,8 @@ import { IHarvestable } from "shared";
 import Harvestable from ".";
 import TreeBody from "shared/bodies/harvestables/TreeBrightBody";
 
-interface Config{
-    x: number;
-    y: number;
-    id: string;
-}
-
 export default class TreeBright extends Harvestable {
-    constructor(scene: Phaser.Scene, config: Config) {
-        const harvestableConfig: IHarvestable = {
-            x: config.x,
-            y: config.y,
-            type: "tree",
-            resource: "wood",
-            id:config.id,
-            health: 100,
-            maxHealth: 100,
-            value: 10,
-        }
+    constructor(scene: Phaser.Scene, harvestableConfig: IHarvestable) {
         //@ts-ignore
         const collisionBox = scene.matter.bodies.rectangle(...TreeBody(harvestableConfig))
         super(scene, harvestableConfig,collisionBox,"Tree_bright");
