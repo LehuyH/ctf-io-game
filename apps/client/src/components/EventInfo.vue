3a5c3413f128033a523da5818272b0db7ce028fa
Lehuy <LeHuy.hoang@outlook.com>
2022-04-29 16:16:28 +0000

    Network EventInfo UI

--- apps/client/src/components/EventInfo.vue
+++ apps/client/src/components/EventInfo.vue
@@ -1,21 +1,60 @@
 <template>
-    <transition enter-active-class="fadeInUp" leave-active-class="fadeOutDown">
-        <section v-if="state.currentEvent"
-         class="bg-black text-white bg-opacity-50 pt-4 fixed z-10 top-[10%] left-4 top-4 rounded shadow-md max-w-xl mx-4">
-         {{state.currentEvent}}
+    <transition enter-active-class="fadeIn" leave-active-class="fadeOut">
+    <section v-if="event"
+         class="bg-black p-4 text-white bg-opacity-50 pt-4 fixed z-10 left-4 top-4 rounded shadow-md max-w-sm mx-4 inline-block overflow-hidden">
+         <h1 class="font-bold text-lg inline-block">{{event.name}}</h1> <span class="font-thin bg-slate-600 px-2">{{timeLeft}} Seconds</span>
+         <br>
+         <p class="marquee overflow-hidden inline-block whitespace-nowrap">{{event.description}}</p>
      </section>
     </transition>
 </template>
 
 <script setup lang="ts">
-import { nextTick, ref, watchEffect } from 'vue';
 import { state } from '~/state';
+import { computed } from 'vue'
+import { IEventInfo } from 'shared';
+const event = computed(()=>{
+    if(state.currentEvent?.id){
+        return state.currentEvent as IEventInfo<any>
+    }else{
+        return null
+    }
+})
+const timeLeft = computed(()=>{
+    if(state.currentEvent?.id){
+        return state.currentEvent.duration/1000 as number
+    }else{
+        return null
+    }
+})
 </script>
 
 <style scoped>
-.progress{
-    transition-property: transform;
-    transform-origin: left;
-    transition-timing-function: linear;
+.marquee{
+    animation: marquee 15s linear infinite;
 }
+@keyframes marquee {
+    0%   { transform: translate(100%, 0); }
+    100% { transform: translate(-110%, 0); }
+}
+
+.fadeIn{
+    animation: fadeIn 1s ease-in-out;
+}
+
+.fadeOut{
+    animation: fadeIn 1s ease-in-out reverse;
+}
+
+@keyframes fadeIn{
+  0% {
+    opacity: 0;
+    transform: translateX(-150%);
+  }
+  100% {
+    opacity: 1;
+    transform: translateX(0%);
+  }
+}
+
 </style>
